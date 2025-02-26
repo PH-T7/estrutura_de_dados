1ce076373505ac0f88e58ecc58a46a0e266c0cdb
PH <140414857+PH-T7@users.noreply.github.com>
2025-02-26 20:03:29 -0300

    Update Pessoa.java

--- Java/filas/Pessoa.java
+++ Java/filas/Pessoa.java
@@ -1,1 +1,16 @@
+package filas;
 
+public class Pessoa {
+    private String nome;
+    private int idade;
+    public Pessoa(String nome, int idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+}
