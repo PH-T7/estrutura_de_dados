2b90c2f8c44be824a7482942f9721e8d2903a114
PH <140414857+PH-T7@users.noreply.github.com>
2025-02-26 20:02:50 -0300

    Add files via upload

---
+++ Java/filas/Fila_Prioridade.java
@@ -0,0 +1,21 @@
+package filas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fila_Prioridade {
+    private  List filaNormal = new ArrayList<Pessoa>();
+    private  List filaPrioridade = new ArrayList<Pessoa>();
+    
+    public void add(Pessoa p){
+
+    }
+
+    
+    public Pessoa remover(){
+        return null;
+    }
+    public int tamanho(){
+        return 0;
+    }
+}
---
+++ Java/filas/Teste_Fila_Prioridade.java
@@ -0,0 +1,5 @@
+package filas;
+
+public class Teste_Fila_Prioridade {
+        
+}
